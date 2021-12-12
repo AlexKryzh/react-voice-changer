137bb5f1e5393a50c848186aa08d0f7fd7da72aa
PLAINCONCEPTS\okryz <okryz@plainconcepts.com>
2021-12-12 14:34:17 +0100

    Add select random Voice

--- src/VoicesHeader/VoicesHeader.tsx
+++ src/VoicesHeader/VoicesHeader.tsx
@@ -1,20 +1,25 @@
 import './VoicesHeader.scss';
 import {ReactComponent as OrderIcon} from 'assets/order.svg';
+import {ReactComponent as RandomIcon} from 'assets/button-random.svg';
 
 type VoicesHeaderProps = {
     sortDesc: boolean;
     onSetSortDesc: Function;
     onSetSearchText: Function;
+    onRandomSelect: Function;
 }
 
 function VoicesHeader(props: VoicesHeaderProps) {
-    const { sortDesc, onSetSortDesc, onSetSearchText } = props;
+    const { sortDesc, onSetSortDesc, onSetSearchText, onRandomSelect } = props;
 
     return (
         <div className="voices-header">
             <input type="text" onChange={(e) => onSetSearchText(e)} />
             <button className="voice__sort btn btn-link"  data-cy="voice-sort" onClick={() => onSetSortDesc(!sortDesc)}>
                 <OrderIcon />
+            </button>
+            <button className="voice__random-select btn btn-link"  data-cy="voice-random" onClick={() => onRandomSelect()}>
+                <RandomIcon />
             </button>
         </div>
     );
